c1d5eaf881b9839d663933381def1f0ad327b332
WMW <wnameless@gmail.com>
2013-01-24 12:44:40 +0800

    Show error messages during schema dump

--- lib/rare_map/schema_reader.rb
+++ lib/rare_map/schema_reader.rb
@@ -4,16 +4,25 @@ module RareMap
   module SchemaReader
     def read_schema(db_profile)
       conn = db_profile.connection.map { |k, v| v.kind_of?(Integer) ? "'#{k}'=>#{v}" : "'#{k}'=>'#{v}'" }.join(', ')
-      if RUBY_PLATFORM == "java"
-        %x[jruby -e "require 'active_record'; require 'activerecord-jdbc-adapter'; ActiveRecord::Base.establish_connection(#{conn}); ActiveRecord::SchemaDumper.dump(ActiveRecord::Base.connection);"]
+      schema = if RUBY_PLATFORM == 'java'
+        %x[jruby -e "require 'active_record'; require 'activerecord-jdbc-adapter'; ActiveRecord::Base.establish_connection(#{conn}); ActiveRecord::SchemaDumper.dump(ActiveRecord::Base.connection);" 2>&1]
       else
-        %x[ruby -e "require 'active_record'; ActiveRecord::Base.establish_connection(#{conn}); ActiveRecord::SchemaDumper.dump(ActiveRecord::Base.connection);"]
+        %x[ruby -e "require 'active_record'; ActiveRecord::Base.establish_connection(#{conn}); ActiveRecord::SchemaDumper.dump(ActiveRecord::Base.connection);" 2>&1]
       end
+      detect_errors schema
+      schema
       # schema = StringIO.new
       # ActiveRecord::Base.establish_connection db_profile.connection
       # ActiveRecord::SchemaDumper.dump(ActiveRecord::Base.connection, schema)
       # schema.close
       # schema.string
+    end
+    
+    def detect_errors(schema)
+      if $? != 0
+        puts schema
+        schema = ''
+      end
     end
   end
 end
