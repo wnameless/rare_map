954b2ff22d9746b96f29b2fccf4e0997eae9467c
WMW <wnameless@gmail.com>
2013-09-11 20:55:52 +0800

    Complete DatabaseProfileTest

--- lib/rare_map/database_profile.rb
+++ lib/rare_map/database_profile.rb
@@ -1,11 +1,29 @@
 require 'rare_map/options'
 
 module RareMap
+  # RareMap::DatabaseProfile defines all metadata of a database.
+  # @author Wei-Ming Wu
+  # @!attribute [r] name
+  #   @return [String] the name of this DatabaseProfile
+  # @!attribute [r] connection
+  #   @return [Hash] the connection config of this DatabaseProfile
+  # @!attribute [r] options
+  #   @return [Options] the Options of this DatabaseProfile
+  # @!attribute schema
+  #   @return [String] the schema dump of this DatabaseProfile
+  # @!attribute tables
+  #   @return [Array] an Array of Table of this DatabaseProfile
   class DatabaseProfile
     attr_reader :name, :connection, :options
     attr_accessor :schema, :tables
     
-    def initialize(name, connection, options)
+    # Creates a DatabaseProfile.
+    #
+    # @param [String] name the name of this DatabaseProfile
+    # @param [Hash] connection the connection config of this DatabaseProfile
+    # @param [Options] options the Options of this DatabaseProfile
+    # @return [DatabaseProfile] a DatabaseProfile object
+    def initialize(name, connection, options = nil)
       @name, @connection = name, connection
       @options = options || Options.new
       @tables = []
---
+++ test/database_profile_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+require 'rare_map/database_profile'
+require 'rare_map/options'
+
+class DatabaseProfileTest < Test::Unit::TestCase
+  def setup
+    @connection = { adapter: 'sqlite3', database: 'db/test.sqlite3' }
+    @options = RareMap::Options.new
+    @db_profile = RareMap::DatabaseProfile.new 'profile1',@connection, @options
+  end
+  
+  def test_constructor
+    assert @db_profile.kind_of? RareMap::DatabaseProfile
+  end
+  
+  def test_properties
+    assert_equal 'profile1', @db_profile.name
+    assert_equal @connection, @db_profile.connection
+    assert_equal @options, @db_profile.options
+    assert_equal nil, @db_profile.schema
+    @db_profile.schema = ''
+    assert_equal '', @db_profile.schema
+    assert_equal [], @db_profile.tables
+  end
+end
